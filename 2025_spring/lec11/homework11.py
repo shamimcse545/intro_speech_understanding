0ce7851bb0635f536e39c5614e549fd73e7ea4eb
Md. Shamim Hossen <81988052+shamimcse545@users.noreply.github.com>
2025-07-28 12:25:46 +0900

    I did homework 11

--- 2025_spring/lec11/homework11.py
+++ 2025_spring/lec11/homework11.py
@@ -5,12 +5,17 @@ def transcribe_wavefile(filename, language='en'):
     Use sr.Recognizer.AudioFile(filename) as the source,
     recognize from that source,
     and return the recognized text.
-    
+
     @params:
     filename (str) - the filename from which to read the audio
     language (str) - the language of the audio (optional; default is English)
-    
+
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    # raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    r = speech_recognition.Recognizer()
+    with speech_recognition.AudioFile(filename) as source:
+        audio = r.record(source)
+        text = r.recognize_google(audio, language=language)
+    return text
